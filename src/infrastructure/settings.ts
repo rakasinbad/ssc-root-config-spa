d592aa151368e8970da1feb626c50d4e741c973e
Reza Raka Nugraha <reza.raka@sinbad.co.id>
2022-09-27 08:19:24 +0700

    update some code

--- src/infrastructure/settings.ts
+++ src/infrastructure/settings.ts
@@ -1,4 +1,3 @@
-
 import { registerApplication } from "single-spa";
 import {
   constructApplications,
@@ -6,19 +5,36 @@ import {
   constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "../microfrontend-layout.html";
+import sscOnly from "../ssc-only.html";
+
+import { ResolvedRoutesConfig } from "single-spa-layout/dist/types/isomorphic/constructRoutes";
 
 export const constructEngine = async () => {
-  const routes = constructRoutes(microfrontendLayout, {
+  const data = {
     loaders: {
       ssc: "<h1>Loading Seller Center</h1>",
-      sidebar: "<h1>Loading Sidebar"
+      sidebar: "<h1>Loading Sidebar",
     },
     errors: {
       ssc: "<h1>Failed to load Seller Center</h1>",
-      sidebar: "<h1>Failed to load Sidebar</h1>"
+      sidebar: "<h1>Failed to load Sidebar</h1>",
     },
-    props: {}
-  });
+    props: {},
+  };
+
+  const currentURL = window.location.href // returns the absolute URL of a page
+
+  const pathname = window.location.pathname //returns the current url minus the domain name
+
+  let routes: ResolvedRoutesConfig;
+
+  if (pathname === '/' || pathname === '/auth/login') {
+    routes = constructRoutes(sscOnly, data);
+  } else {
+    routes = constructRoutes(microfrontendLayout, data);
+  }
+    
+
   const applications = constructApplications({
     routes,
     loadApp({ name }) {
@@ -29,21 +45,21 @@ export const constructEngine = async () => {
   applications.forEach(registerApplication);
 
   return constructLayoutEngine({ routes, applications });
-}
+};
 
 export const initEvent = () => {
-  window.addEventListener("single-spa:before-routing-event", () => {
-      document.getElementById("loading-ring").classList.remove("invisible");
+	window.addEventListener("single-spa:before-routing-event", () => {
+    document.getElementById("loading-ring").classList.remove("invisible");
   });
 
   window.addEventListener("single-spa:routing-event", () => {
-      document.getElementById("loading-ring").classList.add("invisible");
+    document.getElementById("loading-ring").classList.add("invisible");
   });
 
   document.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-          const element = e.target as HTMLElement;
-          element.click();
-      }
+    if (e.key === "Enter") {
+      const element = e.target as HTMLElement;
+      element.click();
+    }
   });
-}
+};
